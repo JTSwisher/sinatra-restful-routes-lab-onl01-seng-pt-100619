8f15c77edd6b7be62904215090431d14af147f00
Jeff Swisher <jefftswisher@gmail.com>
2020-02-04 00:54:18 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,5 +5,37 @@ class ApplicationController < Sinatra::Base
   end
 
   # code actions here!
+  
+  get '/' do 
+    redirect '/recipes'  
+  end
+  
+  get '/recipes' do
+   @recipes = Recipe.all
+   
+   erb :index
+  end 
+  
+  get '/recipes/new' do 
+    erb :new
+  end 
+  
+  get '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    
+    erb :show 
+  end 
+  
+  
+  get '/recipes/:id/edit' do 
+    @recipe = Recipe.find(params[:id])
+    erb :edit 
+  end 
+  
+  post '/recipes' do 
+    @recipe = Recipe.create(params)
+    
+    redirect "/recipes/#{@recipe.id}"
+  end 
 
 end
