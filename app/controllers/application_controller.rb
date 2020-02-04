2443dbc03280b8c0cd8768823cfd08800d3ebd8c
Jeff Swisher <jefftswisher@gmail.com>
2020-02-04 00:59:10 +0000

    Automatically backed up by Learn

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -33,7 +33,13 @@ class ApplicationController < Sinatra::Base
   end 
   
   patch '/recipes/:id' do 
+    @recipe = Recipe.find(params[:id])
+    @recipe.name = params[:name]
+    @recipe.ingredients = params[:ingredients]
+    @recipe.cook_time = params[:cook_time]
+    @recipe.save 
     
+    redirect "/recipes/@recipe.id"
   end 
   
   post '/recipes' do 
